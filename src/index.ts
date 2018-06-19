ba933ae9f0dcb0c56257f58cf401b095ee56c607
Fortune Ekeruo <legalmody@gmail.com>
2018-06-19 18:33:13 +0100

    Use async/await to start server

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import * as path from 'path';
 import { resolvers } from './resolvers';
 import { createConnection } from "typeorm";
 
-export const startServer = () => {
+export const startServer = async () => {
     const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
     const server = new GraphQLServer({ typeDefs, resolvers });
-    createConnection().then(() => server.start(() => {
-        console.log('Server is running on localhost:4000');
-    }));
+    await createConnection();
+    await server.start()
+    console.log('Server is running on localhost:4000');
 }
 
 startServer();
