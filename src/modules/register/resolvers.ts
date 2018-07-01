5a111252de666df0c9e5caddc3685cf7fae304d6
Fortune Ekeruo <legalmody@gmail.com>
2018-07-01 22:05:16 +0100

    Add validation schema with yup

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,6 +1,12 @@
 import * as bcrypt from 'bcryptjs';
+import * as yup from 'yup';
 import { ResolverMap } from '../../types/graphql.utils';
 import { User } from '../../entity/User';
+
+const schema = yup.object().shape({
+    email: yup.string().min(3).max(255).email(),
+    password: yup.string().min(3).max(255)
+});
 
 export const resolvers: ResolverMap = {
     Query: {
