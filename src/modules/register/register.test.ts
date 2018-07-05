8d521e59396eca14adae3a9c3c78cd487c5d5087
Fortune Ekeruo <legalmody@gmail.com>
2018-07-05 12:08:13 +0100

    Refactor register test in a describe block

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -23,67 +23,72 @@ const mutation = (e: string, p: string) => `
     }
 `;
 
-test('Ensures a user is registered', async() => {
-    // Ensures a user is registered
-    const response = await request(getHost(), mutation(email, password));
-    expect(response).toEqual({ register: null });
-    const users = await User.find({ where: { email }});
-    expect(users).toHaveLength(1);
-    const user = users[0];
-    expect(user.email).toEqual(email);
-    expect(user.password).not.toEqual(password);
+describe('Register user', async() => {
+    it('Ensures a user is registered', async() => {
+        const response = await request(getHost(), mutation(email, password));
+        expect(response).toEqual({ register: null });
+        const users = await User.find({ where: { email }});
+        expect(users).toHaveLength(1);
+        const user = users[0];
+        expect(user.email).toEqual(email);
+        expect(user.password).not.toEqual(password);
+    })
 
-    // Ensures duplicate emails should fail
-    const response2: any = await request(getHost(), mutation(email, password));
-    expect(response2.register).toHaveLength(1);
-    expect(response2.register[0]).toEqual({
-        path: 'email',
-        message: duplicateEmail
-    });
+    it('Ensures duplicate emails should fail', async() => {
+        const response2: any = await request(getHost(), mutation(email, password));
+        expect(response2.register).toHaveLength(1);
+        expect(response2.register[0]).toEqual({
+            path: 'email',
+            message: duplicateEmail
+        });
+    })
 
-    // Ensures invalid or short emails should fail
-    const response3: any = await request(getHost(), mutation("f", password));
-    expect(response3).toEqual({
-        register: [
-            {
-                path: 'email',
-                message: emailTooShort,
-            },
-            {
-                path: 'email',
-                message: invalidEmail,
-            }
-        ]
-    });
+    it('Ensures invalid or short emails should fail', async() => {
+        const response3: any = await request(getHost(), mutation("f", password));
+        expect(response3).toEqual({
+            register: [
+                {
+                    path: 'email',
+                    message: emailTooShort,
+                },
+                {
+                    path: 'email',
+                    message: invalidEmail,
+                }
+            ]
+        });
+    })
 
-    // Ensures invalid password should fail
-    const response4: any = await request(getHost(), mutation(email, "fo"));
-    expect(response4).toEqual({
-        register: [
-            {
-                path: 'password',
-                message: invalidPassword,
-            }
-        ]
-    });
+    it('Ensures invalid password should fail', async() => {
+        const response4: any = await request(getHost(), mutation(email, "fo"));
+        expect(response4).toEqual({
+            register: [
+                {
+                    path: 'password',
+                    message: invalidPassword,
+                }
+            ]
+        });
+    })
 
-    // Ensures short and invalid email with invalid password should fail
-    const response5: any = await request(getHost(), mutation("f", "fo"));
-    expect(response5).toEqual({
-        register: [
-            {
-                path: 'email',
-                message: emailTooShort,
-            },
-            {
-                path: 'email',
-                message: invalidEmail,
-            },
-            {
-                path: 'password',
-                message: invalidPassword,
-            }
-        ]
-    });
+    it('Ensures short and invalid email with invalid password should fail', async() => {
+        const response5: any = await request(getHost(), mutation("f", "fo"));
+        expect(response5).toEqual({
+            register: [
+                {
+                    path: 'email',
+                    message: emailTooShort,
+                },
+                {
+                    path: 'email',
+                    message: invalidEmail,
+                },
+                {
+                    path: 'password',
+                    message: invalidPassword,
+                }
+            ]
+        });
+    })
 
 });
