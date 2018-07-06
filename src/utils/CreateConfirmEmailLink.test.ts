59f262165d4b816cecad325f0d4bc0644407c65b
Fortune Ekeruo <legalmody@gmail.com>
2018-07-06 15:48:30 +0100

    Test case that ensure s user's confirmed field is set to true

--- src/utils/CreateConfirmEmailLink.test.ts
+++ src/utils/CreateConfirmEmailLink.test.ts
@@ -1,11 +1,15 @@
 import * as Redis from 'ioredis';
+import fetch from 'node-fetch';
+
 import { createTypeormConn } from "./createTypeormConn";
 import { User } from "../entity/User";
 import { createConfirmEmailLink } from "./CreateConfirmEmailLink";
 
 let userId = '';
+let link: string;
 
 describe('Email Confirmation Link', () => {
+    const redis = new Redis();
     beforeAll(async () => {
         await createTypeormConn();
         const user = await User.create({
@@ -14,14 +18,22 @@ describe('Email Confirmation Link', () => {
         }).save();
     
         userId = user.id;
-    });
-
-    it('Ensures createConfirmationLink works', () => {
-        const link = createConfirmEmailLink(
+        link = await createConfirmEmailLink(
             process.env.TEST_HOST as string,
             userId,
-            new Redis()
-        )
+            redis
+        );
     });
-})
 
+    it('Ensures a user is confirmed when they click the link', async () => {
+        const response = await fetch(link);
+        const text = await response.text();
+        expect(text).toEqual('User confirmed');
+    });
+
+    it('Ensures user confirmed value is set to true', async() => {
+        const user = await User.findOne({ where: { id: userId } });
+        expect((user as User).confirmed).toBeTruthy();
+    })
+
+})
