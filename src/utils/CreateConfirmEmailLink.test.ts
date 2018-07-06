9fe4f74dcff94a6968223224d30b66605b3da2d2
Fortune Ekeruo <legalmody@gmail.com>
2018-07-06 15:18:56 +0100

    Wrap test in a describe block

--- src/utils/CreateConfirmEmailLink.test.ts
+++ src/utils/CreateConfirmEmailLink.test.ts
@@ -3,12 +3,19 @@ import { User } from "../entity/User";
 
 let userId = '';
 
-beforeAll(async () => {
-    await createTypeormConn();
-    const user = await User.create({
-        email: 'fortune@e.com',
-        password: 'pass123'
-    }).save();
+describe('Email Confirmation Link', () => {
+    beforeAll(async () => {
+        await createTypeormConn();
+        const user = await User.create({
+            email: 'fortune@e.com',
+            password: 'pass123'
+        }).save();
+    
+        userId = user.id;
+    });
 
-    userId = user.id;
-});
+    it('Ensures createConfirmationLink works', () => {
+        
+    })
+})
+
