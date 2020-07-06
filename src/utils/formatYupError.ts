291edaff5ca4e4d47e7f3e11bf22defcce57bae5
Fortune Ekeruo <legalmody@gmail.com>
2020-07-06 23:47:59 +0100

    refactor formatYupError util function

--- src/utils/formatYupError.ts
+++ src/utils/formatYupError.ts
@@ -1,12 +1,9 @@
 import { ValidationError } from 'yup';
 
-export const formatYupError = (err: ValidationError) => {
-  const errors: Array<{ path: string; message: string }> = [];
-  err.inner.forEach((e) => {
-    errors.push({
-      path: e.path,
-      message: e.message,
-    });
-  });
-  return errors;
-};
+export const formatYupError = (
+  err: ValidationError
+): Array<{ path: string; message: string }> =>
+  err.inner.map((e) => ({
+    path: e.path,
+    message: e.message,
+  }));
