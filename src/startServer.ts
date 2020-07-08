611c91171cc6868662d6baf5090b57359e7e72e0
Fortune Ekeruo <legalmody@gmail.com>
2020-07-08 09:11:30 +0100

    implement sessions for when a user logs in

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,23 +1,49 @@
 import { GraphQLServer } from 'graphql-yoga';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
 
 import { createTypeormConn } from './utils/createTypeormConn';
 import { redis } from './redis';
 import { confirmEmail } from './routes/confirmEmail';
 import { genSchema } from './utils/generateSchema';
 
+const RedisStore = connectRedis(session);
+
 export const startServer = async () => {
   const server = new GraphQLServer({
     schema: genSchema(),
     context: ({ request }) => ({
       redis,
+      session: request.session,
       url: `${request.protocol}://${request.get('host')}`,
     }),
   });
+
+  server.express.use(
+    session({
+      store: new RedisStore({}),
+      name: 'qid',
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // for 7 days
+      },
+    })
+  );
+
+  const cors = {
+    credentials: true,
+    origin: 'http://localhost:3000',
+  };
 
   server.express.get('/confirm/:id', confirmEmail);
 
   await createTypeormConn();
   const app = await server.start({
+    cors,
     port: process.env.NODE_ENV === 'test' ? 0 : 3001,
   });
   console.log('Server is running on http://localhost:3001');
